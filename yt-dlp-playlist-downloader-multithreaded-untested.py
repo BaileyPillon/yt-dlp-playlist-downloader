6e77687ee23d8cc149062a82df1d2bd3a036ddd8
BaileyPillon <138253619+BaileyPillon@users.noreply.github.com>
2023-07-07 20:29:07 -0400

    Update yt-dlp-playlist-downloader-multithreaded-untested.py

--- yt-dlp-playlist-downloader-multithreaded-untested.py
+++ yt-dlp-playlist-downloader-multithreaded-untested.py
@@ -1,1 +1,62 @@
-# placeholder
+from __future__ import unicode_literals
+import os
+import yt_dlp
+import time
+
+playlist_url = "[Insert playlist URL here]"
+start_time = time.time()
+
+def download_mp3s():
+    # Create a directory to store the downloaded files
+    #os.makedirs("[Insert directory here], exist_ok=True)
+    
+    # Set the options for yt-dlp
+    ydl_opts = {
+        "format": "bestaudio/bestvideo",
+        "ignoreerrors": True,
+        "nooverwrites": True,
+        "writethumbnail": True,
+        "writeinfojson": True,
+        "matchtitle": #"[Name in partial or full to be matchedm, otherwise delete or comment out]",
+        "outtmpl": #"[Insert directory here]/%(title)s.%(uploader)s.%(ext)s",
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "320",
+        }, {
+            "key": "EmbedThumbnail",
+        }]
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([playlist_url])
+
+
+def download_mp4s():
+    # Create a directory to store the downloaded mp4s 
+    os.makedirs("H:/Juice WRLD - Legends Never Die 3", exist_ok=True)
+    
+    # Set the options for yt-dlp
+    ydl_opts = {
+        "format_sort": ['res:bestvideo/bestaudio',"ext:mp4"],
+        "ignoreerrors": True,
+        "nooverwrites": True,
+        "writethumbnail": True,
+        "writeinfojson": True,
+        "matchtitle": "Juice",
+        "outtmpl": "[Insert directory here]/%(title)s.%(uploader)s.%(width)s.%(height)s.%(ext)s",
+    }
+            
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([playlist_url])
+
+if __name__ == "__main__":
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [executor.submit(crawl_media) for _ in range(10)]
+    download_mp4s()
+    download_mp3s()
+    time_taken = time_taken / 60
+    print(f"Time taken: {time_taken:.2f} minutes")
+    time_taken = time_taken / 60
+    print(f"Time taken: {time_taken:.2f} hours")
+
